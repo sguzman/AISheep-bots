f079a10ccb6012792c95a82ce34b8c476a6f0f77
Salvador Guzman <guzmansalv@gmail.com>
2015-04-11 12:38:21 -0700

    info for first round - for testing

---
+++ Tron/starting.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Salvador on 4/11/2015.
+ */
+var board_ = [[null, null, null, null, null, null, null, null, "C", null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, "P", null, null, null, null, null, null, null, null]
+];
+
+var bot_ = [15, 7];
+var other_ = [0, 8];
+
+exports.board = board_;
+exports.bot = bot_;
+exports.other = other_;
